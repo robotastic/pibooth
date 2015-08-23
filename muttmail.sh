05277d75048e0dd0f68ef4c6df644f6c92275617
robotastic <luke@robotastic.com>
2015-08-23 14:16:50 -0400

    debug

--- muttmail.sh
+++ muttmail.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+echo $1
 echo "The kids wanted to send you this photo. We don't check this email addres, so no point in replying." | mutt -s "Berndt Photo Booth" $1 -a test.jpg
 rm test.jpg
 exit 1
